d676eb95c474cdbe9a3b53f210c0219aa449de9b
Adrian Ottiker <adrian@ottiker.com>
2013-09-06 15:17:20 +0200

    created flow.js

---
+++ flow.js
@@ -0,0 +1,19 @@
+var devConfig = {
+    flow: {
+        miid1: {}
+    },
+    miids: {
+        miid1: {},
+        miid2: {},
+        miid3: {}
+    }
+};
+
+function merger () {}
+function repeat () {}
+function binds () {}
+function states () {}
+function events () {}
+function init (config) {}
+
+module.exports = init;
