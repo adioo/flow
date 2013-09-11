8be6dad7bcd31eadc4b72dc31c1dde32a4e15d4e
Adrian Ottiker <adrian@ottiker.com>
2013-09-11 17:41:21 +0200

    thinking about the context of the module

--- flow.js
+++ flow.js
@@ -1,3 +1,50 @@
+var appDescriptor = {
+    // configure event chaining
+    events: {
+        // observer name: miid, global
+        obs: {
+            // listen to eventName
+            eventName: [{
+                // event action
+                emit: {
+                    // emit eventName
+                    eventName: [{
+                        // emit on observer
+                        obs: "miid|global",
+                        // parameter object config
+                        // this is optional
+                        keys: {
+                            // pass this key to the next event
+                            keyName: 1 // 0 | 1
+                        }
+                    }]
+                },
+                
+                // TODO when events had an return value, executing
+                //      a function directly could be obsolete
+                method: {
+                    module: "miid|global",
+                    name: "functionName"
+                },
+                
+                // activate/deactivate states
+                states: {}
+            }]
+        }
+    }
+};
+
+// module methods
+var module = {exports: {}};
+module.exports = function (config) {
+    var self = this;
+    
+    self.on('eventName', function () {});
+};
+
+// custom methods
+
+// what was this??!!
 var devConfig = {
     flow: {
         dom: {
@@ -24,6 +71,25 @@ function repeat () {}
 function binds () {}
 function states () {}
 function events () {}
-function init (config) {}
+
+// module structure
+
+// private methods
+function privateMethod () {}
+
+// public methods
+var methods = {
+    functionName: function () {}
+};
+
+// module init
+function init (config) {
+    var self = this;
+    
+    // listen to public methods
+    for (var name in methods) {
+        self.on(name, methods[name]);
+    }
+}
 
 module.exports = init;
