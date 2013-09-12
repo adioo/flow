07ee5b7edbc0b68d7d2cc3e27c9a0b990d41dc02
Adrian Ottiker <adrian@ottiker.com>
2013-09-12 18:33:07 +0200

    started with the flow module

--- flow.js
+++ flow.js
@@ -1,1 +1,25 @@
-// event flow
+// setup event flow
+function setup (eventFlow) {
+    var self = this;
+}
+
+// listen to methods
+function methodToEvent (methods) {
+    var self = this;
+    
+    for (var event in methods) {
+        if (typeof methods[event] === 'function') {
+            self.on(event, methods[event]);
+        }
+    }
+}
+
+var Flow = {
+    setup: function () {},
+    onFn: methodToEvent
+};
+
+// constructor
+module.exports = function (self) {
+    return Object.extend(Flow, self);
+};
