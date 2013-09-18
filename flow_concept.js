7072e0f13244cf81235b8058ab6963bdc69bbad9
Adrian Ottiker <adrian@ottiker.com>
2013-09-18 17:17:57 +0200

    working on argument passing config

--- flow_concept.js
+++ flow_concept.js
@@ -20,55 +20,28 @@ var externalEventFlow = {
         // TODO what about "once"?
         ready: {
             on: [
-                "methodB"
-                //["eventY", {keyA: 1, keyB: 1}]
+                "eventX",
+                [
+                    "eventY",
+                    [
+                        // param config for argument index 0
+                        { // pass defined keys from an object
+                            keyA: 1,
+                            keyB: 1
+                        },
+                        
+                        // param config for argument index 1
+                        "keyname", // pass one key of an object
+                        
+                        // param config for argument index 2
+                        [0, 4], // pass items of an array
+                        
+                        // param config for argument index 2
+                        2 // pass item of an array
+                    ]
+                ]
             ],
             once: ["testFlow"]
         }
     }
-};
-
-// private methods
-function privateMethod () {
-    var self = this;
-    
-    // emit events from code
-    self.emit('eventName', params);
-}
-
-// public methods
-var publicMethods = {
-    functionName: function () {
-        var self = this;
-        
-        // listen from code
-        // TODO can this be done with a event-flow config?
-        //      it can, but sure there are some cases where
-        //      listen from inside the code is required.
-        self.on('eventName', handlerFunction);
-        self.once('eventName', handlerFunction);
-        
-        // emit events from code
-        self.emit('eventName', params);
-    }
-};
-
-// module init
-module.exports = function (config) {
-    var self = this;
-    var flow = Flow(self);
-    
-    self.config = config;
-    
-    // init flow
-    Flow(self, publicMethods, eventFlow);
-    
-    // TODO how to execute events/methods on init?
-    //      with config..?
-    //      there could be an config to control the
-    //      interal event flow and the "flow" module
-    //      sets up the event structure
-    
-    // emit events from code
-    self.emit('ready');
 };
