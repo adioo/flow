86256b35ebb061039bca136942fb8025b8d6f42d
Adrian Ottiker <adrian@ottiker.com>
2013-09-18 16:03:55 +0200

    chain internal and external event flows

--- flow.js
+++ flow.js
@@ -1,8 +1,55 @@
 M.wrap('github/adioo/flow/vdev/flow.js', function (require, module, exports) {
-// setup event flow
-function setupEventFlow (eventFlow) {
+
+// TODO handle arguments
+function listenHandler (emit) {
+    
+    return function () {
+        var self = this;
+        
+        self.emit(emit);
+    };
+}
+
+function chain (event, obs, emit, i) {
     var self = this;
-    console.log(eventFlow);
+    
+    if (emit.on) {
+        for (i = 0; i < emit.on.length; ++i) {
+            self.on(event, obs, listenHandler(emit.on[i]));
+        }
+    }
+    
+    if (emit.once) {
+        for (i = 0; i < emit.once.length; ++i) {
+            self.once(event, obs, listenHandler(emit.once[i]));
+        }
+    }
+} 
+    
+// setup internal event flow
+function setupInternalEventFlow (eventFlow, obs) {
+    var self = this;
+    
+    obs = obs || self.miid;
+    
+    for (var event in eventFlow) {
+        if (eventFlow.hasOwnProperty(event)) {
+            
+            chain.call(self, event, obs, eventFlow[event]);
+        }
+    }
+}
+
+// setup external event flow
+function setupExternalEventFlow (eventFlow) {
+    var self = this;
+    
+    for (var obs in eventFlow) {
+        if (eventFlow.hasOwnProperty(obs)) {
+            
+            setupInternalEventFlow.call(self, eventFlow[obs], obs);
+        }
+    }
 }
 
 // listen to methods
@@ -17,7 +64,7 @@ function methodToEvent (methods) {
 }
 
 // constructor
-module.exports = function (module, methods, eventFlow) {
+module.exports = function (module, methods, internalEventFlow, externalEventFlow) {
     
     // listen to methods
     if (methods) {
@@ -25,8 +72,13 @@ module.exports = function (module, methods, eventFlow) {
     }
     
     // setup internal event flow
-    if (eventFlow) {
-        setupEventFlow.call(module, eventFlow);
+    if (internalEventFlow) {
+        setupInternalEventFlow.call(module, internalEventFlow);
+    }
+    
+    // setup external event flow
+    if (externalEventFlow) {
+        setupExternalEventFlow.call(module, externalEventFlow);
     }
 };
 
